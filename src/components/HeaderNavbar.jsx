39c520228444c261135fbee9da2f03fea663b7ac
Luca Giampieri <luca.giampieri95@gmail.com>
2026-01-19 16:41:17 +0100

    Aggiunta Navbar dinamica

--- src/components/HeaderNavbar.jsx
+++ src/components/HeaderNavbar.jsx
@@ -1,20 +1,31 @@
 function HeaderNavbar() {
+
+    const navLinks = [
+        { id: 1, label: "CHARACTERS", href: "#" },
+        { id: 2, label: "COMICS", href: "#" },
+        { id: 3, label: "MOVIES", href: "#" },
+        { id: 4, label: "TV", href: "#" },
+        { id: 5, label: "GAMES", href: "#" },
+        { id: 6, label: "COLLECTIBLES", href: "#" },
+        { id: 7, label: "VIDEOS", href: "#" },
+        { id: 8, label: "FANS", href: "#" },
+        { id: 9, label: "NEWS", href: "#" },
+        { id: 10, label: "SHOP", href: "#" }
+    ];
+
+    function NavbarLinkRender() {
+        return navLinks.map((link) => (
+            <li key={link.id}><a className="nav-link" href={link.href}>{link.label}</a></li>
+        ))
+    }
+
     return (
         <nav className="nav-header">
             <figure className="nav-logo">
                 <img src="/dc-logo.png" alt="Logo" />
             </figure>
             <ul className="nav-list">
-                <li><a className="nav-link" href="#">CHARACTERS</a></li>
-                <li><a className="nav-link" href="#">COMICS</a></li>
-                <li><a className="nav-link" href="#">MOVIES</a></li>
-                <li><a className="nav-link" href="#">TV</a></li>
-                <li><a className="nav-link" href="#">GAMES</a></li>
-                <li><a className="nav-link" href="#">COLLECTIBLES</a></li>
-                <li><a className="nav-link" href="#">VIDEOS</a></li>
-                <li><a className="nav-link" href="#">FANS</a></li>
-                <li><a className="nav-link" href="#">NEWS</a></li>
-                <li><a className="nav-link" href="#">SHOP</a></li>
+                {NavbarLinkRender(navLinks)}
             </ul>
         </nav>
     )
